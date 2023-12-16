428e3ab218fa54c101a3b4d9395fca179a2829fa
DeadDuck <46423255+Dead0Duck@users.noreply.github.com>
2023-12-16 13:56:44 +0700

    ✨ Webhook Responding

---
+++ app/webhooks.js
@@ -0,0 +1,19 @@
+module.exports = () => {
+	const express = require("express")
+	const bodyParser = require("body-parser")
+
+	const app = express()
+	const PORT = 3000
+
+	app.use(bodyParser.json())
+
+	app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`))
+
+	app.use(bodyParser.json())
+	app.post("/webhooks/git_update", (req, res) => {
+		res.status(200).end()
+
+		var cp = require('child_process');
+		cp.exec('./startup.sh', () => {})
+	})
+}
