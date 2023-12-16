db91052257e917b1c47bddaba9dea0d7262fdaaf
DeadDuck <46423255+Dead0Duck@users.noreply.github.com>
2023-12-16 09:34:01 +0700

    🐛 Typo

--- app/commands/utility/reload.js
+++ app/commands/utility/reload.js
@@ -1,7 +1,6 @@
 const { SlashCommandBuilder } = require('discord.js');
 
 module.exports = {
-	category: 'utility',
 	data: new SlashCommandBuilder()
 		.setName('reload')
 		.setDescription('Reloads a command.')
