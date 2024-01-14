36b273215d00df5fd90f703f29a620fcaf9542cb
Relitrix <mega.strilets2@gmail.com>
2024-01-14 17:03:44 +0300

    🔐 Дехардкодинг ID разработчика для /eval

--- app/commands/utility/eval.js
+++ app/commands/utility/eval.js
@@ -6,13 +6,13 @@ const clean = async (text) => {
 
 	if (typeof text !== "string")
 		text = require("util").inspect(text, { depth: 1 });
-	
+
 	text = text
 		.replace(/`/g, "`" + String.fromCharCode(8203))
 		.replace(/@/g, "@" + String.fromCharCode(8203));
-	
+
 	return text;
-	}
+}
 
 module.exports = {
 	data: new SlashCommandBuilder()
@@ -26,8 +26,7 @@ module.exports = {
 		.setDefaultMemberPermissions(PermissionFlagsBits.ADMINISTRATOR)
 		.setDMPermission(false),
 	async execute(interaction) {
-		if (interaction.user.id !== '272630656486604800') // Хардкод. Just in case
-		{
+		if (interaction.user.id !== process.env.DEV_ID) {
 			await interaction.reply({ content: 'Nope\nhttps://media1.tenor.com/m/1HOfGCrKQLMAAAAC/sans-undertale.gif', ephemeral: true });
 			return;
 		}
@@ -38,7 +37,7 @@ module.exports = {
 		try {
 			const evaled = eval(code);
 			const cleaned = (await clean(evaled)).substring(0, 1900);
-	
+
 			msg = `\`\`\`js\n${cleaned}\n\`\`\``
 		} catch (err) {
 			const cleaned = (await clean(err)).substring(0, 1900);
