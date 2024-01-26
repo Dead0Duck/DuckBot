58cc4fd432749d018b970a670830cb40fd201104
Relitrix <mega.strilets2@gmail.com>
2024-01-26 18:04:16 +0300

    ✨ Новая команда timeout

---
+++ app/commands/utility/timeout.js
@@ -0,0 +1,58 @@
+const { SlashCommandBuilder, PermissionsBitField } = require('discord.js');
+const { Moderation } = require('../../utils');
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('timeout')
+        .setDescription('Даёт таймаут указанному пользователю.')
+        .setDefaultMemberPermissions(PermissionsBitField.Flags.MuteMembers)
+        .setDMPermission(false)
+        .addUserOption(option =>
+            option
+                .setName("user")
+                .setDescription("Пользователь, которого нужно наказать")
+                .setRequired(true)
+        )
+        .addStringOption(option =>
+            option
+                .setName("duration")
+                .setDescription("Продолжительность мута в формате [число][первая буква единицы времени]")
+                .setRequired(true)
+        )
+        .addStringOption(option =>
+            option
+                .setName("reason")
+                .setDescription("Причина мута")
+                .setRequired(true)
+        )
+        .addStringOption(option =>
+            option
+                .setName("tip")
+                .setDescription("Примечание или описание ситуации.")
+                .setRequired(false)
+        ),
+    async execute(interaction) {
+        const options = interaction.options
+        const member = options.getMember('user')
+        ms = Moderation.parseMSeconds(options.getString('duration'))
+        if (member.id === interaction.applicationId) {
+            return await interaction.reply({ content: "https://media1.tenor.com/m/hghruQWRyi0AAAAC/jojo-i-refuse.gif", ephemeral: true })
+        }
+        if (!ms) {
+            return await interaction.reply({ content: "Ошибка в аргументе продолжительности.", ephemeral: true })
+        }
+        // https://discord.com/developers/docs/resources/guild#modify-guild-member-json-params:~:text=(up%20to%2028%20days%20in%20the%20future)
+        if (ms > 2419200000) {
+            return await interaction.reply({ content: `Вы привысили лимит дискорда. Тайм-аут можно выдать не больше чем на 28 дней.`, ephemeral: true })
+        }
+        if (!member.moderatable) {
+            return await interaction.reply({ content: `У бота нет прав на тайм-аут этого пользователя.`, ephemeral: true })
+        }
+        try {
+            member.timeout(ms, options.getString('reason'))
+            await interaction.reply({ content: `Пользователю <@${member.id}> был выдан тайм-аут на ${Moderation.humanize(ms)}.`, ephemeral: true })
+        } catch (e) {
+            console.error(e)
+        }
+    },
+};
---
+++ app/utils/moderation.js
@@ -0,0 +1,34 @@
+const dayjs = require('dayjs')
+const duration = require('dayjs/plugin/duration')
+const relativeTime = require('dayjs/plugin/relativeTime')
+require('dayjs/locale/ru')
+dayjs.extend(duration)
+dayjs.extend(relativeTime)
+
+const units = new Map()
+
+units.set(['s', 'с'], 1000)
+units.set(['m', 'м'], 60_000)
+units.set(['h', 'ч'], 3_600_000)
+units.set(['w', 'н'], 604_800_000)
+units.set(['y', 'г'], 31_556_952_000)
+
+module.exports = {
+    parseMSeconds: (string) => {
+        if (isNaN(parseInt(string))) {
+            return null
+        }
+        let parsedMSeconds = null
+        units.forEach((ms, key) => {
+            key.forEach((item) => {
+                if (string.toLowerCase().endsWith(item)) {
+                    parsedMSeconds = parseInt(string) * ms
+                }
+            })
+        })
+        return parsedMSeconds
+    },
+    humanize: (ms) => {
+        return dayjs.duration(ms, 'ms').locale('ru').humanize()
+    }
+}
